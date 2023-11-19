d13dbd18c712c1a990fdb946c4e93f03847962d1
unknown <u202012145@hust.edu.cn>
2023-11-19 14:50:59 +0800

    initial

---
+++ src/main/java/ElainaMod/Elaina/Elaina.java
@@ -0,0 +1,72 @@
+package ElainaMod.Elaina;
+
+import ElainaMod.Characters.ElainaC;
+import ElainaMod.cards.Strike;
+import basemod.BaseMod;
+import basemod.interfaces.EditCardsSubscriber;
+import basemod.interfaces.EditCharactersSubscriber;
+import basemod.interfaces.EditStringsSubscriber;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.localization.CharacterStrings;
+
+import static ElainaMod.Characters.ElainaC.Enums.EXAMPLE_CARD;
+import static ElainaMod.Characters.ElainaC.Enums.MY_CHARACTER;
+
+@SpireInitializer
+public class Elaina implements EditStringsSubscriber,EditCardsSubscriber, EditCharactersSubscriber {
+   private static final String MY_CHARACTER_BUTTON = "ElainaMod/img/char/Character_Button.png";
+   // 人物选择界面的立绘
+   private static final String MY_CHARACTER_PORTRAIT = "ElainaMod/img/char/Character_Portrait.png";
+   // 攻击牌的背景（小尺寸）
+   private static final String BG_ATTACK_512 = "ElainaMod/img/512/bg_attack_512.png";
+   // 能力牌的背景（小尺寸）
+   private static final String BG_POWER_512 = "ElainaMod/img/512/bg_power_512.png";
+   // 技能牌的背景（小尺寸）
+   private static final String BG_SKILL_512 = "ElainaMod/img/512/bg_skill_512.png";
+   // 在卡牌和遗物描述中的能量图标
+   private static final String SMALL_ORB = "ElainaMod/img/char/small_orb.png";
+   // 攻击牌的背景（大尺寸）
+   private static final String BG_ATTACK_1024 = "ElainaMod/img/1024/bg_attack.png";
+   // 能力牌的背景（大尺寸）
+   private static final String BG_POWER_1024 = "ElainaMod/img/1024/bg_power.png";
+   // 技能牌的背景（大尺寸）
+   private static final String BG_SKILL_1024 = "ElainaMod/img/1024/bg_skill.png";
+   // 在卡牌预览界面的能量图标
+   private static final String BIG_ORB = "ElainaMod/img/char/card_orb.png";
+   // 小尺寸的能量图标（战斗中，牌堆预览）
+   private static final String ENEYGY_ORB = "ElainaMod/img/char/cost_orb.png";
+   public static final Color MY_COLOR = new Color(79.0F / 255.0F, 185.0F / 255.0F, 9.0F / 255.0F, 1.0F);
+   public Elaina(){
+      BaseMod.subscribe(this);
+      // 这里注册颜色
+      BaseMod.addColor(EXAMPLE_CARD, MY_COLOR, MY_COLOR, MY_COLOR, MY_COLOR, MY_COLOR, MY_COLOR, MY_COLOR,BG_ATTACK_512,BG_SKILL_512,BG_POWER_512,ENEYGY_ORB,BG_ATTACK_1024,BG_SKILL_1024,BG_POWER_1024,BIG_ORB,SMALL_ORB);
+   }
+   public static void initialize(){
+      new Elaina();
+   }
+
+   @Override
+   public void receiveEditCards(){
+      //TODO 写添加卡牌的代码
+      BaseMod.addCard(new Strike());
+   }
+   @Override
+   public void receiveEditCharacters(){
+      BaseMod.addCharacter(new ElainaC(CardCrawlGame.playerName),MY_CHARACTER_BUTTON,MY_CHARACTER_PORTRAIT,MY_CHARACTER);
+   }
+   public void receiveEditStrings() {
+      String lang;
+      if (Settings.language == Settings.GameLanguage.ZHS) {
+         lang = "ZHS"; // 如果语言设置为简体中文，则加载ZHS文件夹的资源
+      } else {
+         lang = "ENG"; // 如果没有相应语言的版本，默认加载英语
+      }
+      BaseMod.loadCustomStringsFile(CardStrings.class, "ElainaMod/localization/" + lang + "/cards.json"); // 加载相应语言的卡牌本地化内容。
+      // 如果是中文，加载的就是"ExampleResources/localization/ZHS/cards.json"
+      BaseMod.loadCustomStringsFile(CharacterStrings.class, "ElainaMod/localization/" + lang + "/characters.json");
+   }
+}

babe683643620a9d574d0846668d31520102ffb0
unknown <u202012145@hust.edu.cn>
2023-11-19 21:05:14 +0800

    添加了新的凑数卡，但还是没办法通过战斗结算（需要三张C，三张U，三张R）

---
+++ src/main/java/ElainaMod/cards/SuperDefend.java
@@ -0,0 +1,50 @@
+package ElainaMod.cards;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Defend extends CustomCard {
+    public static final String ID = "Elaina:Defend";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "ElainaMod/img/cards/Defend.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = CardColor.COLORLESS;
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public Defend() {
+        // 为了命名规范修改了变量名。这些参数具体的作用见下方
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseBlock = 5;
+        this.tags.add(CardTags.STARTER_DEFEND);
+    }
+
+    @Override
+    public void upgrade() { // 升级调用的方法
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBlock(3);
+        }
+    }
+    /**
+     * 当卡牌被使用时，调用这个方法。
+     *
+     * @param p 你的玩家实体类。
+     * @param m 指向的怪物类。（无指向时为null，包括攻击所有敌人时）
+     */
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // AbstractCard中实现了addToBot方法，它的效果和AbstractDungeon.actionManager.addToBottom相同
+        this.addToBot(new GainBlockAction(p,p,this.block));
+    }
+}
---
+++ src/main/java/ElainaMod/cards/SuperStrikeDefend.java
@@ -0,0 +1,50 @@
+package ElainaMod.cards;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Strike extends CustomCard {
+    public static final String ID = "Elaina:Strike";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
+    private static final String NAME = CARD_STRINGS.NAME;
+    private static final String IMG_PATH = "ElainaMod/img/cards/Strike.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardColor COLOR = CardColor.COLORLESS;
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+
+    public Strike() {
+        // 为了命名规范修改了变量名。这些参数具体的作用见下方
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.damage = this.baseDamage = 6;
+        this.tags.add(CardTags.STARTER_STRIKE);
+        this.tags.add(CardTags.STRIKE);
+    }
+
+    @Override
+    public void upgrade() { // 升级调用的方法
+        if (!this.upgraded) {
+            this.upgradeName(); // 卡牌名字变为绿色并添加“+”，且标为升级过的卡牌，之后不能再升级。
+            this.upgradeDamage(3); // 将该卡牌的伤害提高3点。
+        }
+    }
+    /**
+     * 当卡牌被使用时，调用这个方法。
+     *
+     * @param p 你的玩家实体类。
+     * @param m 指向的怪物类。（无指向时为null，包括攻击所有敌人时）
+     */
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // AbstractCard中实现了addToBot方法，它的效果和AbstractDungeon.actionManager.addToBottom相同
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL)));
+    }
+}

61b7e44f4651a4d6ff1dd773b8d2278cab3e97f6
86138 <907928237@qq.com>
2023-12-16 15:33:10 +0800

    新增示弱，添置新装

---
+++ src/main/java/ElainaMod/cards/ShowWeakness.java
@@ -0,0 +1,49 @@
+package ElainaMod.cards;
+
+import ElainaMod.Characters.ElainaC;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+
+public class ShowWeakness extends AbstractElainaCard {
+    public static final String ID = "Elaina:ShowWeakness";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
+    private static final String IMG_PATH = "ElainaMod/img/cards/ShowWeakness.png";
+    private static final int COST = 0;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public ShowWeakness() {
+        // 为了命名规范修改了变量名。这些参数具体的作用见下方
+        super(ID, CARD_STRINGS, IMG_PATH, COST, TYPE, RARITY, TARGET);
+        this.cardsToPreview = new Reversal();
+        this.exhaust = true;
+    }
+
+    @Override
+    public void upgrade() { // 升级调用的方法
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
+            this.initializeDescription();
+            this.cardsToPreview.upgrade();
+        }
+    }
+    /**
+     * 当卡牌被使用时，调用这个方法。
+     *
+     * @param p 你的玩家实体类。
+     * @param m 指向的怪物类。（无指向时为null，包括攻击所有敌人时）
+     */
+    @Override
+    public void BasicEffect(ElainaC p, AbstractMonster m) {
+        // AbstractCard中实现了addToBot方法，它的效果和AbstractDungeon.actionManager.addToBottom相同
+        this.addToBot(new ApplyPowerAction(p,p,new VulnerablePower(p,1,false)));
+        this.addToBot(new MakeTempCardInHandAction(this.cardsToPreview.makeStatEquivalentCopy()));
+    }
+}

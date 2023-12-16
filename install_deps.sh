099655081260402522f022aa2529b33d0937003a
hatrd <onnyx@foxmail.com>
2023-12-17 01:35:21 +0800

    tag testv0.0.0

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 mkdir -p jar && cd jar
 wget https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/BaseMod.jar https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/desktop-1.0.jar https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/ModTheSpire.jar https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/StSLib.jar
-pwd && ls && echo "所有文件已下载完成。"

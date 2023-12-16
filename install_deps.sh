83788405b4d43caecbd8058ba657b8720884763e
NaN03 <73582053+hatrd@users.noreply.github.com>
2023-12-17 01:15:23 +0800

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# 创建jar目录并进入
-mkdir -p jar && cd jar &
-# 并发下载所需的JAR文件
-curl -O https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/BaseMod.jar &
-curl -O https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/desktop-1.0.jar &
-curl -O https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/ModTheSpire.jar &
-curl -O https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/StSLib.jar &
-# 等待所有后台任务完成
-wait
-# 输出完成信息
-echo "所有文件已下载完成。"
+mkdir -p jar && cd jar
+wget https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/BaseMod.jar https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/desktop-1.0.jar https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/ModTheSpire.jar https://github.com/fe11n/ElainaInSpire/releases/download/build_deps/StSLib.jar
+pwd && ls && echo "所有文件已下载完成。"

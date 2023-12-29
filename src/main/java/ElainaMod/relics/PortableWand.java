ac844c0ed107c2a7b024bc644eee9f9c65764afc
fe11n <130979206+fe11n@users.noreply.github.com>
2023-12-29 20:10:30 +0800

    更新

--- src/main/java/ElainaMod/relics/PortableWand.java
+++ src/main/java/ElainaMod/relics/PortableWand.java
@@ -22,7 +22,7 @@ public class PortableWand extends CustomRelic {
     }
     public void atTurnStart() {
         ElainaC p = (ElainaC) AbstractDungeon.player;;
-        if(p.getConclusion().isInstant){//如果使用tag判断INSTANT会导致更改INSTANT时同类卡牌全部INSTANT被修改
+        if(p.getConclusion()!=null && p.getConclusion().isInstant){//如果使用tag判断INSTANT会导致更改INSTANT时同类卡牌全部INSTANT被修改
             this.flash();
             this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
             p.getConclusion().InstantUse();

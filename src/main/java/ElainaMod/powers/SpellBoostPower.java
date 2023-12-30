f8b098a9880d88ee5907d077c75e304d6c333a6a
hatrd <onnyx@foxmail.com>
2023-12-30 16:44:27 +0800

    Bugfix: 扩大排除范围，适配联机模组

--- src/main/java/ElainaMod/powers/SpellBoostPower.java
+++ src/main/java/ElainaMod/powers/SpellBoostPower.java
@@ -43,10 +43,6 @@ public class SpellBoostPower extends AbstractPower {
                 return;
             }
             owner.getPower("Elaina:SpellLink").flash();
-            StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-            for (StackTraceElement element : stackTrace) {
-                System.out.println(element.toString());
-            }
             logger.info("gainblock "+num);
             addToBot(new GainBlockAction(owner,num));
         }

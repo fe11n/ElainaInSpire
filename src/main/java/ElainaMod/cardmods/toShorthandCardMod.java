3a4e80996cc1bb7d13c62e382196a146ffd863c5
NaN03 <73582053+hatrd@users.noreply.github.com>
2024-03-31 10:36:27 +0800

    CI&CD: 临时解决找不到 addToBot 符号

--- src/main/java/ElainaMod/cardmods/toShorthandCardMod.java
+++ src/main/java/ElainaMod/cardmods/toShorthandCardMod.java
@@ -4,10 +4,12 @@ import ElainaMod.action.RecordCardAction;
 import ElainaMod.cards.AbstractElainaCard;
 import basemod.abstracts.AbstractCardModifier;
 import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 public class toShorthandCardMod extends AbstractCardModifier {
     private String postDescription;
@@ -44,7 +46,14 @@ public class toShorthandCardMod extends AbstractCardModifier {
         }//只允许对不为速记的卡添加该mod
         card.rawDescription = postDescription;
     }
-
+    
+    // Github 工作流上编译时找不到父类的 addToBot 符号，这里直接实现以临时解决
+    @Override
+    public void addToBot(AbstractGameAction action) {
+        AbstractDungeon.actionManager.addToBottom(action);
+    }
+    
+    @Override
     public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
         if(!(card instanceof AbstractElainaCard)){
             this.addToBot(new RecordCardAction(card));
